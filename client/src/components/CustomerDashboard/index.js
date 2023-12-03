6e22acc6c2c2409ba4f7daf5729c50f68719cad2
eesso123 <25100297@lums.edu.pk>
2023-12-04 02:51:59 +0500

    Added Feedback and Search Functionality

--- client/src/components/CustomerDashboard/index.js
+++ client/src/components/CustomerDashboard/index.js
@@ -1,14 +1,23 @@
 import axios from "axios";
 import React, { useCallback, useEffect, useState } from "react";
 
-import { Button, Card, CardBody, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, CardBody, Col, Container, Row, Form } from "react-bootstrap";
 import api from "../../api";
+import StarRatings from 'react-star-ratings';
+
+
+
 
 const CustomerDashboard = () => {
+  const [searchQuery, setSearchQuery] = useState('');
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
   const [orders, setOrders] = useState([]);
   const customerID = localStorage.getItem("userID");
+  const [feedbacks, setFeedbacks] = useState(() => {
+  const savedFeedbacks = localStorage.getItem('feedbacks');
+  return savedFeedbacks ? JSON.parse(savedFeedbacks) : {};
+  });
   const token = localStorage.getItem("token");
 
   const fetchProducts = useCallback(async () => {
@@ -92,11 +101,126 @@ const CustomerDashboard = () => {
     }
   };
 
-  return (
-    <Container>
-      <h1>Customer Dashboard</h1>
-      <Row xs={1} md={2} lg={3} className="g-4">
-        {products.map((product) => (
+  const handleFeedbackChange = (orderId, productId, field, value) => {
+    setFeedbacks({
+      ...feedbacks,
+      [orderId]: {
+        ...feedbacks[orderId],
+        [productId]: {
+          ...feedbacks[orderId]?.[productId],
+          [field]: value,
+          submitted: feedbacks[orderId]?.[productId]?.submitted || false,
+        },
+      },
+    });
+  };
+
+  // Adjusted function to submit feedback for a product
+ const submitFeedback = async (orderId, productId) => {
+  const currentFeedback = feedbacks[orderId]?.[productId];
+  if (!currentFeedback) {
+    alert("Please provide feedback before submitting.");
+    return;
+  }
+
+  if (currentFeedback.submitted) {
+    alert("Feedback for this product has already been submitted.");
+    return;
+  }
+
+  try {
+    await axios.post(
+      `${api}/feedback/submit`,
+      {
+        oid: orderId,
+        pid: productId,
+        supplier_rating: currentFeedback.supplier_rating,
+        service_rating: currentFeedback.service_rating,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    // Update the feedbacks state with the submitted feedback
+    const updatedFeedbacks = {
+      ...feedbacks,
+      [orderId]: {
+        ...feedbacks[orderId],
+        [productId]: {
+          ...currentFeedback,
+          submitted: true, // Set the submitted flag to true
+        },
+      },
+    };
+    
+    setFeedbacks(updatedFeedbacks);
+
+    // Save the updated feedbacks to localStorage
+    localStorage.setItem('feedbacks', JSON.stringify(updatedFeedbacks));
+
+    alert('Feedback submitted successfully!');
+  } catch (error) {
+    console.error("Error submitting feedback:", error);
+    alert("Failed to submit feedback.");
+  }
+};
+
+const styles = {
+  card: {
+    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+    transition: '0.3s',
+    marginBottom: '20px',
+  },
+  cardBody: {
+    padding: '16px',
+  },
+  button: {
+    margin: '5px',
+  },
+  searchInput: {
+    marginBottom: '20px',
+  },
+  feedbackButton: {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: '10px',
+  },
+  feedbackText: {
+    marginLeft: '10px',
+    color: 'green',
+  },
+};
+
+
+
+
+return (
+  <Container>
+    <h1>Customer Dashboard</h1>
+
+    {/* Search Input */}
+    <Form.Group controlId="productSearch" style={styles.searchInput}>
+      <Form.Control 
+        type="text" 
+        placeholder="Search Products" 
+        value={searchQuery} 
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </Form.Group>
+
+    <Row xs={1} md={2} lg={3} className="g-4">
+      {products
+        .filter(product => 
+          product.name.toLowerCase().includes(searchQuery.toLowerCase().trim())
+        )
+        .sort((a, b) => 
+          a.name.toLowerCase() === searchQuery.toLowerCase().trim() ? -1 : 
+          b.name.toLowerCase() === searchQuery.toLowerCase().trim() ? 1 : 0
+        )
+        .map((product) => (
           <Col key={product._id}>
             <Card className="mb-3">
               <Card.Body>
@@ -110,28 +234,27 @@ const CustomerDashboard = () => {
             </Card>
           </Col>
         ))}
-      </Row>
-
-      {cart.length > 0 && (
+    </Row>
+    {cart.length > 0 && (
         <div>
           {cart.map((item) => (
-            <Card key={item._id} className="cart-item mb-2">
-              <Card.Body>
+            <Card key={item._id} style={styles.card}>
+              <CardBody style={styles.cardBody}>
                 <Card.Title>{item.name}</Card.Title>
                 <Button
                   variant="danger"
-                  className="remove-from-cart-btn"
+                  style={styles.button}
                   onClick={() => removeFromCart(item._id)}
                 >
                   Remove from Cart
                 </Button>
-              </Card.Body>
+              </CardBody>
             </Card>
           ))}
-
           <Button
             className="place-order-btn mt-3"
             variant="success"
+            style={styles.button}
             onClick={placeOrder}
           >
             Place Order
@@ -139,25 +262,64 @@ const CustomerDashboard = () => {
         </div>
       )}
 
+      {/* Orders and Feedback Section */}
       {orders.length > 0 && (
         <div>
           <h2>Your Orders</h2>
           {orders.map((order) => (
-            <Card
-              key={order._id}
-              style={{
-                marginBottom: "10px",
-              }}
-            >
-              <CardBody>
-                <p>Order ID: {order._id}</p>
-                <p>Date: {new Date(order.date).toLocaleDateString()}</p>
-                <p>Total Amount: ${order.amount}</p>
-              </CardBody>
-            </Card>
+            <Card key={order._id} style={styles.card}>
+              <CardBody style={styles.cardBody}>
+          {/* Order details */}
+          {order.products.map((product) => (
+            <div key={product._id}>
+              <h5>{product.name}</h5>
+              <Form>
+                {/* Supplier Rating */}
+                <Form.Group>
+                  <Form.Label>Supplier Rating</Form.Label>
+                  <StarRatings
+                    rating={feedbacks[order._id]?.[product._id]?.supplier_rating || 0}
+                    starRatedColor="blue"
+                    changeRating={(newRating) => handleFeedbackChange(order._id, product._id, 'supplier_rating', newRating)}
+                    numberOfStars={5}
+                    name='supplier_rating'
+                  />
+                </Form.Group>
+
+                {/* Service Rating */}
+                <Form.Group>
+                  <Form.Label>Service Rating</Form.Label>
+                  <StarRatings
+                    rating={feedbacks[order._id]?.[product._id]?.service_rating || 0}
+                    starRatedColor="blue"
+                    changeRating={(newRating) => handleFeedbackChange(order._id, product._id, 'service_rating', newRating)}
+                    numberOfStars={5}
+                    name='service_rating'
+                  />
+                </Form.Group>
+
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <Button 
+                    variant="primary" 
+                    onClick={() => submitFeedback(order._id, product._id)}
+                    disabled={feedbacks[order._id]?.[product._id]?.submitted}
+                  >
+                    Submit Feedback
+                  </Button>
+                  {feedbacks[order._id]?.[product._id]?.submitted && (
+                    <span style={{ marginLeft: '10px', color: 'green' }}>
+                      Feedback Submitted
+                    </span>
+                  )}
+                </div>
+              </Form>
+            </div>
           ))}
-        </div>
-      )}
+        </CardBody>
+      </Card>
+    ))}
+  </div>
+)}
     </Container>
   );
 };
